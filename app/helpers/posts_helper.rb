2b40c8ffb7eeadc660c58f336b32520ff71c17b7
Monika Zygmunt <monika.a.zygmunt@gmail.com>
2017-09-19 14:48:03 +0000

    added helpers

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,19 @@
 module PostsHelper
+    
+    def post_date(post)
+        post.created_at.in_time_zone("Europe/Warsaw").strftime('%d-%m-%Y %H:%M:%S')
+    end
+    
+    def post_comments(post)
+        link_to "#{I18n.t('post.comment')} #{post.comments.size}", post
+    end    
+    
+    def post_new
+        if current_user.username == "admin" 
+           "#{I18n.t('post.admin')}"  
+        else 
+           "#{I18n.t('post.new')}"  
+        end
+    end    
+    
 end
--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -1,2 +1,5 @@
 module WordsHelper
+    def word_pair(pair)
+        "- #{Word.find(pair.second_id).content}" if Word.exists?(pair.second_id)
+    end  
 end
